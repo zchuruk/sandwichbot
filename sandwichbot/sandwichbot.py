aff8a05dc775a3bcb0ebb2c12146c6fd0106bcbb
zchuruk <zchurukian@gmail.com>
2020-12-09 01:19:35 -0800

    Add debug printout for top N sandwiches

--- sandwichbot/sandwichbot.py
+++ sandwichbot/sandwichbot.py
@@ -11,6 +11,16 @@ SIGMA_D = 1.0
 SIGMA_H = 1.0
 SIGMA_R = 1.0
 
+class MaxVals:
+    def __init__(self, k):
+        self.k = k
+        self.max_vals = [(-100, np.zeros(len(Sandwich)))] * k
+
+    def add_value(self, value, x_arr):
+        if value > self.max_vals[self.k-1][0]:
+            self.max_vals[self.k-1] = (value, copy.copy(x_arr))
+            self.max_vals = sorted(self.max_vals, key=lambda x: x[0], reverse=True)
+
 def fd(deliciousness_value):
     # deliciousness function
     return deliciousness_value
@@ -38,9 +48,8 @@ def satisfaction(x, D, H, R):
     S = SIGMA_D * fd(deliciousness_agg) + SIGMA_H * fh(heaviness_agg) + SIGMA_R * fr(reliability_agg)
     return S
 
-def rec_optimize(x_curr, n, D, H, R, max_val_in, x_max_in):
-    x_max = x_max_in
-    max_val = max_val_in
+def rec_optimize(x_curr, n, D, H, R, max_vals_in):
+    max_vals = max_vals_in
     for i in range(len(x_curr)):
         x = copy.copy(x_curr)
         x[i] += 1
@@ -48,21 +57,18 @@ def rec_optimize(x_curr, n, D, H, R, max_val_in, x_max_in):
             val = satisfaction(x, D, H, R)
             # print(val)
             # print(x)
-            if val > max_val:
-                x_max = copy.copy(x)
-                max_val = val
+            max_vals.add_value(val, x)
         else:
-            max_val, x_max = rec_optimize(x, n, D, H, R, max_val, x_max)
+            max_vals = rec_optimize(x, n, D, H, R, max_vals)
     # print(max_val)
-    return max_val, x_max
+    return max_vals
 
 def run_optimization(n, D, H, R):
     x0 = np.zeros(len(Sandwich))
-    max_val = -100
-    x_max = x0
+    max_vals = MaxVals(5)
 
-    max_val, x_max = rec_optimize(x0, n, D, H, R, max_val, x_max)
-    return x_max
+    max_vals = rec_optimize(x0, n, D, H, R, max_vals)
+    return max_vals
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A bot to select sandwiches.')
@@ -73,5 +79,12 @@ if __name__ == "__main__":
 
     D, H, R = make_taste_matrices(args.people)
 
-    best_sandwiches = run_optimization(args.num_sandwiches, D, H, R)
-    print(best_sandwiches)
+    max_vals = run_optimization(args.num_sandwiches, D, H, R)
+    for i, val_x_pair in enumerate(max_vals.max_vals):
+        val = val_x_pair[0]
+        x = val_x_pair[1]
+        print('{}: {} | {}'.format(i, val,x))
+        print('Deliciousness: {}'.format(SIGMA_D * fd(np.mean(np.matmul(D, x)) / len(x))))
+        print('Heaviness: {}'.format(SIGMA_H * fh(np.mean(np.matmul(H, x)) / len(x))))
+        print('Reliability: {}'.format(SIGMA_R * fr(np.mean(np.matmul(R, x)) / len(x))))
+        
