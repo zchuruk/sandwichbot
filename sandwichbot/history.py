12b94bfa971c33958ea99db591cb205109ff1138
Jeremy Bogle <jbogle@apple.com>
2020-12-09 00:59:21 -0800

    [history] implement add history script

--- sandwichbot/history.py
+++ sandwichbot/history.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import argparse
 import pandas as pd
 import numpy as np
 from datetime import datetime
@@ -19,17 +20,18 @@ def init_history(name):
   headers = [today()] + list(map(lambda s: s.get_name(), list(Sandwich)))
   pd.DataFrame(data=np.array([headers])).to_csv(filename, mode='a', index=False, header=False)
 
-def add_history(name, sando):
+def add_history(name, sandos):
   # Initialize history file
   filename = history_filename(name)
   if (not os.path.exists(filename)):
     init_history(name)
 
   # Append new row to history
+  sandwiches_eaten = list(map(Sandwich.from_name, sandos))
   history = pd.read_csv(history_filename(name))
   new_row = [today()]
   for col in history.columns[1:]: 
-    if (Sandwich.from_name(col) == Sandwich.from_name(sando)):
+    if (Sandwich.from_name(col) in sandwiches_eaten):
       new_row.append(1)
     else:
       new_row.append(0)
@@ -43,6 +45,14 @@ def get_history_matrix(names, days):
     group_history.append(last_n_days)
   print(np.array(group_history))
 
-# init_history('zach')
-add_history('zach', 'v')
-# get_history_matrix(['pat'], 2)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Add history for a users sandwiches.')
+    parser.add_argument('num_people', type=int, help='The number of people who ate sandos today')
+    args = parser.parse_args()
+    
+    for i in range(args.num_people):
+      name = input("Identify yourself: ")
+      sandos = input("Which sandos did you eat today?: ").split(',')
+      add_history(name, sandos)
+
+    print("Thank you for your service")
